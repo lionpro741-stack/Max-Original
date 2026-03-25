bf24ea27533316eca7886bb33b85c71df5adfc12
lionpro741-stack <lionpro741@gmail.com>
2026-03-25 16:32:48 +0200

    create classes

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
-print("Hello")
-print("Привет, как дела?")
-print("Урааа работаетахахахаххаа")
-print("ахахахахахаха")
+import uvicorn
+from fastapi import FastAPI, Depends, Form, Request, Response
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from starlette.responses import HTMLResponse
+
+# 1. База данных
+DB_URL = "sqlite:///./users.db"
+engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id=Column(Integer,primary_key=True,index=True)
+    username = Column(String)
+    number = Column(Integer,unique=True)
+    adders = Column(String)
+    avatar = Column(String)
+
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get('/register_page')
+def
+
+
+
+
+
+
